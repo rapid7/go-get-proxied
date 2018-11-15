ebfabb8477e476ddc96ed0d2d264a9b9b847845a
Taylor Osmun <Taylor_Osmun@rapid7.com>
2018-11-15 16:30:20 -0500

    Updating gofmt

--- .travis.gofmt.sh
+++ .travis.gofmt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! $(gofmt -l $(find ./ -name '*.go' | grep -v vendor)) ] ; then
+if [ ! -z "$(gofmt -l $(find ./ -name '*.go' | grep -v vendor))" ] ; then
    gofmt -d $(find ./ -name '*.go' | grep -v vendor)
    echo "Go code is not formatted. Please run \"gofmt -w \$(find ./ -name '*.go' | grep -v vendor)\""
    exit 1
