52fa4d7a3410d937b3121e3eecad49e64148f2e9
Taylor Osmun <Taylor_Osmun@rapid7.com>
2018-11-15 16:11:32 -0500

    Adding gofmt check

---
+++ .travis.gofmt.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ -n "$(gofmt $(find ./ -name '*.go' | grep -v vendor))" ] ; then
+   gofmt -d $(find ./ -name '*.go' | grep -v vendor)
+   echo "Go code is not formatted. Please run \"gofmt -w \$(find ./ -name '*.go' | grep -v vendor)\""
+   exit 1
+fi
